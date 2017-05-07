87251de78a505edebf3825e7a56b5b7d004f1ee9
Brian Cui <brian.cui@live.com>
2017-05-06 23:15:37 -0500

    Renamed test.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+<<README
 
-########## Variables
+This script creates symbolic links from dotfiles in your home directory
+to any and all of the dotfiles (incl. directories) stored in this repository.
+Old dotfiles will get backed up to ~/dotfiles_old before they are overwritten.
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # get the absolute location of this shell script, where the synced dotfiles are
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile emacs emacs.d"    # list of files/folders to symlink in homedir
+README
 
-##########
+
+# $dir is the absolute location of this shell script, where the synced dotfiles are
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+olddir=~/dotfiles_old # old dotfiles backup directory
+cd $dir
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo "Creating $olddir to backup existing dotfiles."
 mkdir -p $olddir
-echo "done"
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
+# iterate through all dotfiles
+for file in .[^.]* ; do
+    # filter out common but unwanted dotfiles
+    if [ ".git" != $file ] \
+        && [ ".DS_STORE" != $file ] \
+        && [ ".osx" != $file ]; then
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir/ # will print a (nonfatal) error for dotfiles that don't exist yet
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+        # backup existing dotfile (or symlink?) if any
+        if [ -e ~/$file ] || [ -L ~/$file ]; then
+            echo "Moving old $file into to $olddir."
+            # may need to delete before moving, cannot replace directory with non-directory
+            rm -rf $olddir/$file
+            mv ~/$file $olddir/
+        fi
+
+        # link dotfile from home -> $dir
+        echo "Creating symlink to $file in home directory."
+        ln -s $dir/$file ~/$file
+    fi
 done
