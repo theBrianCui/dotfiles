8171918bc9f1c2da6f06aef80c68d350219110a7
Brian Cui <brian.cui@live.com>
2017-05-06 23:12:12 -0500

    Perfected the script

--- test.sh
+++ test.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
+<<README
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # get the absolute location of this shell script, where the synced dotfiles are
-olddir=~/dotfiles_old             # old dotfiles backup directory
+This script creates symbolic links from dotfiles in your home directory
+to any and all of the dotfiles (incl. directories) stored in this repository.
+Old dotfiles will get backed up to ~/dotfiles_old before they are overwritten.
 
+README
+
+
+# $dir is the absolute location of this shell script, where the synced dotfiles are
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+olddir=~/dotfiles_old # old dotfiles backup directory
 cd $dir
 
 # create dotfiles_old in homedir
 echo "Creating $olddir to backup existing dotfiles."
 mkdir -p $olddir
 
+# iterate through all dotfiles
 for file in .[^.]* ; do
-    echo $file
+    # filter out common but unwanted dotfiles
     if [ ".git" != $file ] \
         && [ ".DS_STORE" != $file ] \
         && [ ".osx" != $file ]; then
 
-        if [ -e ~/$file ]; then
+        # backup existing dotfile (or symlink?) if any
+        if [ -e ~/$file ] || [ -L ~/$file ]; then
             echo "Moving old $file into to $olddir."
+            # may need to delete before moving, cannot replace directory with non-directory
+            rm -rf $olddir/$file 
             mv ~/$file $olddir/
         fi
 
+        # link dotfile from home -> $dir
         echo "Creating symlink to $file in home directory."
         ln -s $dir/$file ~/$file
     fi
