022a8fdd0fb001b2add0ed5c3c84b73cde1ad174
Brian Cui <brian.cui@live.com>
2018-01-18 14:27:32 -0600

    First pass at quickstart script

---
+++ quickstart.sh
@@ -0,0 +1,23 @@
+cd ~
+
+# install git and essentials
+sudo apt-get install -y git curl emacs
+git config --global user.name "Brian Cui"
+git config --global user.email "brian.cui@live.com"
+
+# install node.js
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# update packages
+sudo apt-get update -y
+
+# setup git ssh keys
+ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
+read -p "Press [Enter] to continue"
+
+# setup dotfiles
+git clone git@github.com:theBrianCui/dotfiles.git
+cd dotfiles
+./setup.sh
