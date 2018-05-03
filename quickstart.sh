c4a013824f287b073aadce2f86e755e6e083695e
Brian Cui <theBrianCui@users.noreply.github.com>
2018-05-03 13:13:10 -0500

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -4,6 +4,7 @@ cd ~
 # install git and essentials
 sudo apt-get install -y git curl xclip dos2unix
 sudo apt-get install -y emacs
+sudo apt-get install -y python-pip python-dev python-virtualenv
 sudo apt-get install -y python3-pip python3-dev python-virtualenv
 sudo apt-get install -y default-jre default-jdk
 git config --global user.name "Brian Cui"
