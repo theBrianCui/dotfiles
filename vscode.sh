ebca79ffca5d558db7e8cc95e011db5431313649
Brian Cui <brian.cui@live.com>
2018-08-16 15:44:04 -0700

    simplified vscode install step

--- vscode.sh
+++ vscode.sh
@@ -2,4 +2,4 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
-sudo apt-get install code
+sudo apt-get install code -y
